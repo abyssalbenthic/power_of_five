7392b9cca4ee4c401265a2dfd46de77f237aaba7
pilhoon <pilhoon@gmail.com>
2013-11-13 01:02:02 +0900

    modify echonumber.c 's output format

--- poweroffive/echonumber.c
+++ poweroffive/echonumber.c
@@ -2,6 +2,7 @@
 #include <gmp.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
 bool is_this_echo(char* r) {
 	if(strlen(r) %2 != 0)
@@ -32,10 +33,13 @@ main(int argc, const char * argv[])
         mpz_get_str(r, 10, n);
 		mpz_mod_ui(k, n, 100000000000);
 		if(mpz_cmp_ui(k, 0) == 0)
+        {
             gmp_printf("%Zd\n", i);
+            printf("\033[A\033[2K");
+        }
         
 		if(is_this_echo(r))
-			printf("%s\n", r);
+			printf("\n\n%s\n\n", r);
     }
     
     mpz_clear(n);
